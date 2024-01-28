cc78934075aa2117dd0a79ca1daded3b6308c73e
Rick Toews <rick.toews@gmail.com>
2024-01-27 19:40:14 -0800

    Add publication year to displayed book information.

--- src/Book.js
+++ src/Book.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Book.css';
 
 const Book = ({ book }) => {
-    const { Title, 'Author (Last, First)': author, Summary, Genre, ISBN, Shelf } = book;
+    const { Title, 'Author (Last, First)': author, 'Year Published': yearPublished, Summary, Genre, ISBN, Shelf } = book;
     const coverUrl = ISBN ? `http://covers.openlibrary.org/b/isbn/${ISBN}-M.jpg` : '';
 
     return (
@@ -12,16 +12,17 @@ const Book = ({ book }) => {
             </div>
             <div className="book-info">
                 <div className="title-author">
-                    <strong>{Title}</strong><br/>
+                    <strong>{Title}</strong><br />
                     <span style={{ fontSize: 'medium' }}>by {author}</span>
                 </div>
                 <div className="summary" style={{ fontSize: 'small' }}>
                     {Summary}
                 </div>
                 <div className="genre">
-                    <strong>Genre:</strong> {Genre}<br/>
-                    <strong>Location:</strong> {Shelf}<br/>
-                    <strong>ISBN:</strong> {ISBN}<br/>
+                    <strong>Genre:</strong> {Genre}<br />
+                    <strong>Published:</strong> {yearPublished}<br />
+                    <strong>Location:</strong> {Shelf}<br />
+                    <strong>ISBN:</strong> {ISBN}<br />
                 </div>
             </div>
         </div>
